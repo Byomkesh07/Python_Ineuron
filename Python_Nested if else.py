711fd31552b0134e817df30435f9b7a5c96d0753
Byomkesh07 <117578118+Byomkesh07@users.noreply.github.com>
2022-11-06 18:52:26 +0530

    Create Python_Nested if else.py

---
+++ Python_Nested if else.py
@@ -0,0 +1,14 @@
+#Declaring and assign the variables
+
+a=45
+
+#conditions applied
+
+if a>=80 and a<=100:
+  print ("Grade A")
+elif a>=60 and a<=79:
+  print ("Grade B")
+elif a>=40 and a<=59:
+        print ("Grade C")
+else:
+    print ("Fail")    
