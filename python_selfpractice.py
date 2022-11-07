20819dafc9502e3f3635540b10fb2d327e1f2780
Byomkesh07 <117578118+Byomkesh07@users.noreply.github.com>
2022-11-07 16:22:56 +0530

    Create python_selfpractice.py

---
+++ python_selfpractice.py
@@ -0,0 +1,41 @@
+#Declaring & assinging values
+
+# int_Number = int(input("Enter the Number = "))
+# print ("Number = ",int_Number)
+
+# #condition
+
+# if (int_Number==0):
+#  print ("Null value")
+# elif (int_Number>0):
+#  print ("Positive Value")
+# else:
+#  print ("Negetive Value")
+
+# int_a=int(input("Enter the first no.: "))
+# int_b=int(input("Enter the Second no.: "))
+# int_c=int(input("Enter the Third no.: "))
+
+# #Mathematical functions
+
+# float_d=(int_a+int_b+int_c)/3
+# print("The average of three no is = ",float_d)
+
+# int_a=int(input("Enter the no.: "))
+# if (int_a==2):
+#     print("This is an even prime no.")
+# elif (int_a%2==0):
+#     print("This is not a prime no.")
+# else:
+#     print("This is a prime no.")    
+
+# float_r=float(input("Enter the circle radius: "))
+# float_a=(3.142*(float_r**2))
+# float_c=(2*3.142*float_r)
+# print("The area of the circle is = ",float_a," And The circumference of the circle is float_c= ",float_c)
+
+# int_y=int(input("Enter the year: "))
+# if (int_y%4==0 and int_y%400==0 or int_y%100!=0):
+#     print ("This year ",int_y," is leap year")
+# else:
+#     print ("This year ",int_y," is not leap year") 
